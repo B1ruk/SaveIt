637b1f43b2484ed6e2235b8f22ccebdc1907ec83
Biruk Tesfaw <Biruk3870@gmail.com>
2018-07-16 08:22:08 +0300

    Remove Caligraphy library
    -The lib was never used

--- app/src/main/java/io/start/biruk/saveit/App.java
+++ app/src/main/java/io/start/biruk/saveit/App.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import io.start.biruk.saveit.di.component.DaggerMainComponent;
 import io.start.biruk.saveit.di.component.MainComponent;
 import io.start.biruk.saveit.di.module.MainModule;
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
  * Created by biruk on 5/22/2018.
@@ -17,11 +16,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                .setDefaultFontPath("fonts/Bazer.ttf")
-                .setFontAttrId(R.attr.fontPath)
-                .build()
-        );
 
         appComponent= DaggerMainComponent.builder()
                 .mainModule(new MainModule(this))
