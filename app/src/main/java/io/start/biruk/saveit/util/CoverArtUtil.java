1a4f7b6a27d8361287f75f64f845beefb33ea20d
Biruk Tesfaw <Biruk3870@gmail.com>
2018-07-19 13:03:15 +0300

    Refactor the TagAdapter by delegating artwork retrieval to a util class
    -Create a new util class for cover art retrieval
    -Refactor the TagViewAdapter and its view

---
+++ app/src/main/java/io/start/biruk/saveit/util/CoverArtUtil.java
@@ -0,0 +1,23 @@
+package io.start.biruk.saveit.util;
+
+import com.annimon.stream.Stream;
+
+import java.io.File;
+import java.util.List;
+
+import io.start.biruk.saveit.model.db.ArticleModel;
+
+/**
+ * Created by biruk on 19/07/18.
+ */
+
+public class CoverArtUtil {
+
+    public static List<String> getCoverArtPath(List<ArticleModel> articleModels) {
+        return Stream.of(articleModels)
+                .map(ArticleModel::getPath)
+                .filter(path -> new File(path + File.separator + "sc.png").exists())
+                .toList();
+    }
+
+}
--- app/src/main/java/io/start/biruk/saveit/view/tagsView/adapter/TagViewAdapter.java
+++ app/src/main/java/io/start/biruk/saveit/view/tagsView/adapter/TagViewAdapter.java
@@ -25,21 +25,22 @@ import io.start.biruk.saveit.R;
 import io.start.biruk.saveit.events.TagSelectEvent;
 import io.start.biruk.saveit.model.data.TagData;
 import io.start.biruk.saveit.model.db.ArticleModel;
+import io.start.biruk.saveit.util.CoverArtUtil;
 import io.start.biruk.saveit.view.tagsView.TagView;
 
 /**
  * Created by biruk on 5/29/2018.
  */
-public class TagViewAdapter extends RecyclerView.Adapter<TagViewAdapter.TagViewHolder>{
+public class TagViewAdapter extends RecyclerView.Adapter<TagViewAdapter.TagViewHolder> {
 
     @Inject Picasso picasso;
 
     private List<TagData> tags;
     private TagView.TagListener tagListener;
 
-    public TagViewAdapter(TagView.TagListener tagListener){
+    public TagViewAdapter(TagView.TagListener tagListener) {
         this.tagListener = tagListener;
-        this.tags=new ArrayList<>();
+        this.tags = new ArrayList<>();
 
         App.getAppComponent().inject(this);
     }
@@ -58,28 +59,22 @@ public class TagViewAdapter extends RecyclerView.Adapter<TagViewAdapter.TagViewH
 
     @Override
     public void onBindViewHolder(TagViewHolder holder, int position) {
-        String tag = tags.get(position).getTag();
-        int size = tags.get(position).getArticleModels().size();
+        TagData tagData = tags.get(position);
 
-        String sizeTxt="";
-        if (size==1){
-            sizeTxt=size+ " article";
-        }else {
-            sizeTxt=size+ "articles";
+        List<String> coverArtPath = CoverArtUtil.getCoverArtPath(tagData.getArticleModels());
+        if (!coverArtPath.isEmpty()) {
+            picasso.load(new File(coverArtPath.get(0) + File.separator + "sc.png"))
+                    .fit()
+                    .into(holder.tagArticleCoverArt);
         }
+
+        String tag = tagData.getTag();
+        int size = tagData.getArticleModels().size();
+
+        String sizeTxt = size > 1 ? size + " articles " : size + " article";
 
         holder.tagTitleView.setText(tag);
         holder.tagArticleCountView.setText(sizeTxt);
-
-        List<String> coverArts = Stream.of(tags.get(position).getArticleModels())
-                .map(ArticleModel::getPath)
-                .filter(path -> new File(path + File.separator + "sc.png").exists())
-                .toList();
-
-        picasso.load(new File(coverArts.get(0)+File.separator + "sc.png"))
-                .placeholder(R.drawable.default_bg)
-                .resize(140,140)
-                .into(holder.tagArticleCoverArt);
     }
 
     @Override
@@ -87,14 +82,14 @@ public class TagViewAdapter extends RecyclerView.Adapter<TagViewAdapter.TagViewH
         return tags.size();
     }
 
-    class TagViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    class TagViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @Bind(R.id.tag_content_cover) ImageView tagArticleCoverArt;
         @Bind(R.id.tag_title) TextView tagTitleView;
         @Bind(R.id.tag_article_count) TextView tagArticleCountView;
 
         public TagViewHolder(View itemView) {
             super(itemView);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
             itemView.setOnClickListener(this);
         }
 
